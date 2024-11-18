93ad530322e871faedde7ebd2c3ca229358b26d2
Kenzo <541231107@student.smktelkom-pwt.sch.id>
2024-11-18 15:32:59 +0700

    finishing filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function Product()
+    {
+        return $this->HasMany(Product::class);
+    }
 }
